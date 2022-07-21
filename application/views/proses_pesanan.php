b0c61d60d12e673f7470e9760975c607279a375f
mutiararmd <85858617+mutiararmd@users.noreply.github.com>
2022-07-21 18:59:08 +0700

    Delete application directory

--- application/views/proses_pesanan.php
+++
@@ -1,8 +0,0 @@
-<div class="container-fluid">
-	<div class="alert alert-success" role="alert">
-		<h4 class="alert-heading">Terima Kasih!</h4>
-		<p>Pesanan anda sudah masuk ke database kami! :)</p>
-		<hr>
-		<!--  <p class="mb-0">Whenever you need to, be sure to use margin utilities to keep things nice and tidy.</p> -->
-	</div>
-</div>
